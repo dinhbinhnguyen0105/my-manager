61b5f4279e13909120d331e8989bb14bd9e1d711
dinhbinhnguyen-main <dinhbinhnguyen.main@gmail.com>
2024-09-25 15:20:15 +0700

    from macbook

--- src/views/sidebar/sidebar.py
+++ src/views/sidebar/sidebar.py
@@ -11,7 +11,7 @@ MY_DIR = os.path.abspath(os.path.join(__file__, os.path.pardir))
 MAIN_DIR = os.path.abspath(os.path.join(MY_DIR, os.path.pardir,os.path.pardir,os.path.pardir))
 ICONS_DIR = os.path.abspath(os.path.join(MAIN_DIR, "assets", "icons"))
 
-from ..logics import selector
+from ..utils import selector
 
 class Sidebar(QFrame):
     def __init__(self, parent=None):
@@ -23,30 +23,48 @@ class Sidebar(QFrame):
         main_layout.setAlignment(Qt.AlignTop)
         self.setLayout(main_layout)
 
+        self.current_btn_index = 0
+        self.home_btn_widget = QPushButton(self)
+        self.home_btn_widget.setProperty("class", "button activated btn-store")
+        self.home_btn_widget.setObjectName("btn-store")
+        self.home_btn_widget.setIcon(QIcon(os.path.join(ICONS_DIR, "home.svg")))
+        self.home_btn_widget.setIconSize(QSize(26, 26))
+
         self.store_btn_widget = QPushButton(self)
         self.store_btn_widget.setProperty("class", "button btn-store")
-        self.store_btn_widget.setIcon(QIcon(os.path.join(ICONS_DIR, "home.svg")))
+        self.store_btn_widget.setObjectName("btn-store")
+        self.store_btn_widget.setIcon(QIcon(os.path.join(ICONS_DIR, "store.svg")))
         self.store_btn_widget.setIconSize(QSize(26, 26))
 
         self.robot_btn_widget = QPushButton(self)
         self.robot_btn_widget.setProperty("class", "button btn-robot")
+        self.store_btn_widget.setObjectName("btn-robot")
         self.robot_btn_widget.setIcon(QIcon(os.path.join(ICONS_DIR, "robot.svg")))
         self.robot_btn_widget.setIconSize(QSize(26, 26))
 
-        self.robot_btn_widget.clicked.connect(lambda : print("Clicked"))
+        self.store_btn_widget.clicked.connect(lambda : self.handle_click(self.home_btn_widget))
+        self.store_btn_widget.clicked.connect(lambda : self.handle_click(self.store_btn_widget))
+        self.robot_btn_widget.clicked.connect(lambda : self.handle_click(self.robot_btn_widget))
 
-
-
-        # buttons = selector.find_widgets_by_class(self, QPushButton, "button")
-        # print(buttons)
-
+        main_layout.addWidget(self.home_btn_widget)
         main_layout.addWidget(self.store_btn_widget)
         main_layout.addWidget(self.robot_btn_widget)
 
+
         with open(f"{MY_DIR}/sidebar.styles.qss", "r") as f:
-            styles = f.read()
-        self.setStyleSheet(styles)
+            self.my_styles = f.read()
+        self.setStyleSheet(self.my_styles)
     
-    def handle_click(self):
-        pass
+    def handle_click(self, current_btn_widget):
+        button_widgets = selector.find_widgets_by_class(self, QPushButton, "button")
+        activated_button_widget = selector.find_widgets_by_class(self, QPushButton, "activated")[0]
+        selector.remove_class(activated_button_widget, "activated")
+        
+        for index, button_widget in enumerate(button_widgets):
+            if button_widget != current_btn_widget: selector.remove_class(button_widget, "activated")
+            else:
+                selector.add_class(button_widget, "activated")
+                self.current_btn_index = index
+
+        self.setStyleSheet(self.my_styles)
 
--- src/views/utils/selector.py
+++ src/views/utils/selector.py
@@ -8,7 +8,12 @@ def find_widgets_by_class(parent_widget, widget_object,class_name):
 def find_widget_by_id(parent_widget, widget_object, id):
     return parent_widget.findChild(widget_object, id)
 
-def add_class(widget):
-    
-    pass
-def remove_class(widget): pass
+def add_class(widget, class_name):
+    _class = widget.property("class")
+    _class += f" {class_name}"
+    widget.setProperty("class", _class)
+
+def remove_class(widget, class_name): 
+    _class = widget.property("class")
+    _class = _class.replace(f" {class_name}", "")
+    widget.setProperty("class", _class)
